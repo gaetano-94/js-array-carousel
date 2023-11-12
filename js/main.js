3c14e801ed8ccd9cce916c0bcb7cf3e0caa2e889
gaetano-94 <gaetano.panico1994@gmail.com>
2023-11-12 10:53:07 +0100

    feat: variabili per elementi del dom

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 'use strict';
+
+//variabili per gli elementi nel DOM
+const images = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg'];
+const items = document.querySelector('items');
+const prev = document.querySelector('prev');
+const next = document.querySelector('next');
